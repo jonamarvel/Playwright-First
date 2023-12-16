6f5a1485541aa7592720a17c42f3dd8a40a10e27
jonamarvel <112841973+jonamarvel@users.noreply.github.com>
2023-12-16 12:05:53 +1100

    Marathon assignment2 completed

--- tests/week3_Marathon/chatterNewCase.spec.ts
+++ tests/week3_Marathon/chatterNewCase.spec.ts
@@ -18,34 +18,34 @@ test("Salesforce : To create a Lead and convert into Opportunity",async ({page})
 
    //To click App Launcher icon
    await page.locator('.slds-icon-waffle').click();
-   await page.waitForTimeout(1000);
+
+   expect(page.locator('.slds-icon-waffle')).toBeVisible();
+
+   await page.waitForTimeout(2000);
 
    //To click View All 
-   await page.waitForSelector('[aria-label="View All Applications"]');
-
    await page.locator('[aria-label="View All Applications"]').click();
 
-     //To goto Search app textbox and enter Service and press enter
-     await page.getByPlaceholder('Search apps or items...').fill('Service');
-     page.keyboard.press("Enter");
+   //To goto Search app textbox and enter Service and press enter
+   await page.getByPlaceholder('Search apps or items...').fill('Service');
+   page.keyboard.press("Enter");
   
-     await page.waitForTimeout(1000);
+   await page.waitForTimeout(1000);
 
-     //To click on Service Tile
-     await page.locator('div[data-name="Service"]').click();
+  //To click on Service Tile
+  await page.locator('div[data-name="Service"]').click();
 
-     //To click Cases Tab
-     await page.locator('a[title="Cases"]').click();
+  //To click Cases Tab
+  await page.locator('a[title="Cases"]').click();
 
-     //To click on New button
-     await page.locator('div[title ="New"]').click();
+  //To click on New button
+  await page.locator('div[title ="New"]').click();
 
-     //To click on Contact Name field to search contacts
-     //await page.getByPlaceholder("Seach Contacts...").click();
-     await page.locator('input[placeholder="Search Contacts..."]').click();
+  //To click on Contact Name field to search contacts
+  await page.locator('input[placeholder="Search Contacts..."]').click();
 
-    //To click on New Contact from list
-    await page.locator('lightning-base-combobox-item[data-value="actionCreateNew"]').click();
+  //To click on New Contact from list
+  await page.locator('lightning-base-combobox-item[data-value="actionCreateNew"]').click();
 
     //To Click on Salutation
     await page.locator('div[class^="salutation"] a[class="select"]').click();
@@ -57,19 +57,21 @@ test("Salesforce : To create a Lead and convert into Opportunity",async ({page})
     await page.getByPlaceholder("First Name").fill("Aaron");
 
     //To Enter Last Name
-    await page.getByPlaceholder("Last Name").fill("Aaron")
+    await page.getByPlaceholder("Last Name").fill("James")
 
     //To click on Save button
     await page.locator('button[title="Save"]').click();
 
-    const toastMessage=await page.locator('div[class="toastContent slds-notify__content"]').innerText();
+    //To verify the toast message for Contact successful creation
+    const contactToastMessage=await page.locator('div[class="toastContent slds-notify__content"]').innerText();
+    expect(contactToastMessage).toContain("Contact was created");
+    console.log(contactToastMessage);
+    expect(page.locator('div[class="toastContent slds-notify__content"]')).toBeHidden;
 
-    expect(toastMessage).toContain("Contact was created");
+    await page.waitForTimeout(1000);
 
-    console.log(toastMessage);
-
-    //To Click on Account Name input field to search Accounts
-    await page.locator('input[placeholder="Search Accounts..."]').click();
+  //To Click on Account Name input field to search Accounts
+  await page.locator('input[placeholder="Search Accounts..."]').click();
 
     await page.waitForTimeout(2000);
 
@@ -77,7 +79,7 @@ test("Salesforce : To create a Lead and convert into Opportunity",async ({page})
     await page.locator('span[title="New Account"]').click();
 
     //To fill Account Name
-    await page.locator('input[class=" input"]').first().fill("Royal Limited");
+    await page.locator('input[class=" input"]').first().fill("RoyalA Limited");
 
     //To fill Account Number
     await page.locator('input[class=" input"]').nth(3).fill("001");
@@ -91,84 +93,101 @@ test("Salesforce : To create a Lead and convert into Opportunity",async ({page})
     //To click on Save button
     await page.locator('button[title="Save"]').click();
 
-    //To verify toast message
+    await page.waitForTimeout(2000);
+
+    //To verify toast message for Account successful creation
     const accountToastMessage = await page.locator('span[class="toastMessage slds-text-heading--small forceActionsText"]').innerText();
-    expect(accountToastMessage).toContain("Royal Limited");
+    expect(accountToastMessage).toContain("Account");
+    console.log(accountToastMessage);
+    expect (page.locator('span[class="toastMessage slds-text-heading--small forceActionsText"]')).toBeHidden();
 
-    //To click on Status - New
-    await page.locator('button[aria-label="Status, New"]').click();
+  //To click on Status - New
+  await page.locator('button[aria-label="Status, New"]').click();
 
-    //To click on Priority dropdown
-    await page.locator('button[aria-label="Priority, Medium"]').click();
+  //To click on Priority dropdown
+  await page.locator('button[aria-label="Priority, Medium"]').click();
 
-    //To select High for Priority dropdown
-    await page.locator('span[title="High"]').click();
+  //To select High for Priority dropdown
+  await page.locator('span[title="High"]').click();
 
-    //To click on Case Origin
-    await page.locator('button[aria-label="Case Origin, --None--"]').click();
+  //To click on Case Origin
+  await page.locator('button[aria-label="Case Origin, --None--"]').click();
 
-    //To select Email for Case Origin
-    await page.locator('span[title="Email"]').click();
+  //To select Email for Case Origin
+  await page.locator('span[title="Email"]').click();
 
-    //To fill Subject
-    await page.locator('input[name="Subject"]').fill("Product Return Request");
+  //To fill Subject
+  await page.locator('input[name="Subject"]').fill("Product Return Request");
 
-    //To fill Description
-    await page.locator('textarea[class="slds-textarea"]').first().fill("Requesting a return for a defective product");
+  //To fill Description
+  await page.locator('textarea[class="slds-textarea"]').first().fill("Requesting a return for a defective product");
 
-    //To click on Save button
-    await page.locator('button[name="SaveEdit"]').click();
+  //To click on Save button for New Case creation
+  await page.locator('button[name="SaveEdit"]').click();
 
-    //To get a Case successfull creation toast message
-    const caseToastMessage = await page.locator('span[class^="toastMessage"]').innerText();
-    //To verify toast message 
-    expect(caseToastMessage).toContain("was created");
+  //To verify toase message for Case successfull creation
+  const caseToastMessage = await page.locator('span[class^="toastMessage"]').innerText();
+  expect(caseToastMessage).toContain("was created");
+  console.log(caseToastMessage);
+  await page.waitForTimeout(2000);
+  expect (page.locator('span[class^="toastMessage"]')).toBeHidden();
 
-    //To Edit Status field in Details section
-    await page.locator('button[title="Edit Status"]').click();
+  //To Edit Status field in Details section
+  await page.locator('button[title="Edit Status"]').click();
 
-    //To select Escalated from Status dropdown
-    await page.locator('span[title="Escalated"]').click();
+  //To select Escalated from Status dropdown
+  await page.locator('button[aria-label="Status, New"]').click();
+  await page.locator('span[title="Escalated"]').click();
 
-    //To click on Save button
-    await page.locator('button[name="SaveEdit"]').click();
+  //To click on Save button
+  await page.locator('button[name="SaveEdit"]').click();
 
-    //To verify updated value in Status field
-    const updatedStatus = await page.locator('lightning-formatted-text[slot="outputField"]').first().innerText();
+  await page.waitForTimeout(2000);
+
+  //To verify updated value in Status field
+  const updatedStatus = await page.locator('p[class^="fieldComponent"]').nth(1).innerText();
+  console.log(`Updated Status of the Case is ${updatedStatus}`);
+  expect(updatedStatus).toContain("Escalated");
+
+  //To click on Share an update field
+  await page.locator('button[title="Share an update..."]').click();
+
+  //To fill valid data in Share an update field
+  await page.locator('div[data-placeholder="Share an update..."]').fill("Hello! New Case has been created!");
+
+  //To click on Share button
+  await page.locator('button[title="Click, or press Ctrl+Enter"]').click();
+
+  //To verify that Shared message appeared in All updates section
+  const sharedMessage = await page.locator('div[class="feedBodyInner Desktop oneApp"] span[class="uiOutputText"]').first().innerText();
+  expect(sharedMessage).toContain("Hello!");
+  console.log(sharedMessage);
     
-    expect(updatedStatus).toContain("Escalated");
+  //To click on dropdown icon to choose Like on Chatter option
+  await page.locator('a[class^="cuf-feedItemActionTrigger"] lightning-icon[icon-name="utility:down"] svg[data-key="down"]').first().click();
 
-    //To click on Share an update field
-    await page.locator('button[title="Share an update..."]').click();
+  //To click on Like on Chatter option
+  await page.locator('li[title="Like on Chatter"] a[role="menuitem"]').first().click();
 
-    //To fill valid data in Share an update field
-    await page.locator('div[data-placeholder="Share an update..."]').fill("Hello! New Case has been created!");
+  //To verify post liked toast message
+  const postLikedToastMessage = await page.locator('span[class="toastMessage slds-text-heading--small forceActionsText"]').innerText();
+  expect(postLikedToastMessage).toContain("Post was liked.");
+  console.log(postLikedToastMessage);
 
-    //To click on Share button
-    await page.locator('button[title="Click, or press Ctrl+Enter"]').click();
+  //To click on Chatter Tab
+  await page.locator('a[title="Chatter"]').click();
 
-    //To verify that Shared message appeared in All updates section
-    const sharedMessage = await page.locator('div[class="feedBodyInner Desktop oneApp"] span[class="uiOutputText"]').first().innerText();
-    expect(sharedMessage).toContain("Hello!");
+  await page.waitForTimeout(1000);
 
-    //To click on dropdown icon to choose Like on Chatter option
-    await page.locator('div[class="cuf-media-right"]').click();
+  //To search for the liked post in Search this feed input box
+  await page.getByRole('tab',{name: "Post"}).click();
+  await page.locator('input[aria-label="Search this feed..."]').last().fill(sharedMessage);
+  await page.keyboard.press("Enter");
 
-    //To click on Like on Chatter option
-    await page.locator('li[title="Like on Chatter"]').first().click();
-
-    //To verify post liked toast message
-    const postLikedToastMessage = await page.locator('span[class="toastMessage slds-text-heading--small forceActionsText"]').innerText();
-    expect(postLikedToastMessage).toContain("Post was liked.");
-
-    //To click on Chatter Tab
-    await page.locator('a[title="Chatter"]').click();
-
-    
-
-
-
-
+  //To verify post liked by user in Chatter Tab
+  const chatterPostliked = await page.getByLabel("Jona Marvel Nayagam").locator('div[class^="cuf-feedBodyText"] span[dir="ltr"]').innerText();
+  console.log(chatterPostliked);
+  expect(chatterPostliked).toBe(sharedMessage);
 
 
 
