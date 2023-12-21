421e2d9e3794c9d1be808c6d0db25a2c401fa3dd
jonamarvel <112841973+jonamarvel@users.noreply.github.com>
2023-12-21 22:37:48 +1100

    04_HA_SFuploadFiles.spec.ts is added

--- tests/week4_assignments/04_HA_SFuploadFiles.spec.ts
+++ tests/week4_assignments/04_HA_SFuploadFiles.spec.ts
@@ -41,13 +41,13 @@ await page.locator(".slds-icon-waffle").click();
     }
 //To wait for page to load
 await page.waitForLoadState("load");
-// 3.Search for Accounts
+//To Search for Accounts
 await page.locator('input[class="slds-input"]').fill("Accounts");
-// 4.Click the Accounts
+//To Click the Accounts
 await page.locator('a[class="al-menu-item slds-p-vertical--xxx-small"]').click();
-// 5.Click on the first resulting account name
+//To Click on the first resulting account name
 await page.locator('a[data-aura-class="forceOutputLookup"]').first().click();
-// 6.Upload files under Notes and Attachments 
+//To Upload files under Notes and Attachments 
 const filechooserPromise = page.waitForEvent('filechooser', {timeout: 5000});
 await page.getByRole('button', { name: 'Upload Files' }).click();
 const fileChooser = await filechooserPromise;
@@ -55,16 +55,16 @@ await page.waitForTimeout(2000);
 await fileChooser.setFiles(path.join(__dirname,'Salesforce_CheatSheets.pptx'));
 await page.waitForTimeout(6000);
 await page.getByRole('button', {name: "Done"}).click();
+//To Verify the toast message 
 const toastMessage = await page.locator('span[class^="toastMessage"]').innerText();
 console.log(`Toast message is : ${toastMessage}`);
 expect(toastMessage).toBe("1 file was added to the Account.");
 console.log("Toast message is verified");
+//To verify uploaded file name
 const uploadedFileName = page.locator('div[class^="slds-show_inline-block"] span[data-aura-class="uiOutputText"]').first();
 expect(uploadedFileName).toContainText("Salesforce_CheatSheets");
 console.log(`\nYou have successfully uploaded the file : ${await uploadedFileName.innerText()}`);
 console.log("File is uploaded successfully"); 
-//await page.focus('div[class="slds-file-selector__dropzone"]');
-//   Hint : use page.focus(selector) to scroll to view the element - uploadfiles 
-// 7.Verify the toast message 
+
+
 })
-//1 file was added to the Account.
